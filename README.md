eef2ce067d793abb2347876867c56f2e31273afd
Xymb <xymb@endcrystal.me>
2022-08-29 04:17:34 +0200

    First version.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# linear_region_file_format
+# Linear region file format for Minecraft
+
+Linear region format saves about 60% of disk space in OW and Nether and 95% in The End.
+This repository hosts tools to convert between `.mca` and `.linear`.
+
+Usage:
+./mca_to_linear.py r.0.0.mca
+Will create r.0.0.linear
+
+./linear_to_mca.py r.0.0.linear
+Will create r.0.0.mca
---
+++ linear_to_mca.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys
+import zlib
+import time
+
+from linear import Chunk, Region, open_region_linear, write_region_anvil
+
+filename = sys.argv[1]
+filename_anvil = filename.rpartition('.')[0] + ".mca"
+print("Converting " + filename + " to " + filename_anvil)
+
+region = open_region_linear(filename)
+write_region_anvil(filename_anvil, region, compression_level=zlib.Z_DEFAULT_COMPRESSION)
