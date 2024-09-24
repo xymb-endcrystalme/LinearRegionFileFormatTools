0de77d3e3c8532ab9c264e6deb93b2a66635ba6f
Xymb <xymb@endcrystal.me>
2024-09-24 20:09:19 +0200

    a

--- tools/experiment_block_unwrapping_experiment_double_sections.py
+++ tools/experiment_block_unwrapping_experiment_double_sections.py
@@ -18,9 +18,6 @@ def get_file_size(file_path):
     return os.path.getsize(file_path)
 
 def main():
-    # Precompute the Hilbert curve
-    hilbert3d_precompute()
-
     if False:
         INPUT_FILE = "/tmp/r.0.0.mca"
         region = mclinear.open_region_anvil(INPUT_FILE)
@@ -37,10 +34,80 @@ def main():
 
         nbt = chunk.as_nbtlib()
 
+        total_palette = {}
+        '''
         for section in nbt["sections"]:
 #            if section["Y"] == 1:
-            if "block_states" in section and "data" in section["block_states"] and len(section["block_states"]["palette"]) > 1:
-                pass
+
+            if "block_states" in section and "data" in section["block_states"] and len(section["block_states"]["palette"]) >= 1:
+                for element in section["block_states"]["palette"]:
+                    s = str(element)
+                    if s not in total_palette:
+                        total_palette[s] = element
+
+        print("Total palette length:", len(total_palette))
+        '''
+        TO_MERGE = 2
+
+        offset = 0
+
+        for i, section in enumerate(nbt["sections"]):
+            if int(section["Y"] <= -5):
+                offset += 1
+                continue
+            if "block_states" not in section or "data" not in section["block_states"] or len(section["block_states"]["palette"]) <= 1:
+                print(section["Y"])
+                break
+
+            if (i - offset) % TO_MERGE == 0: previous_sections = []
+            previous_sections.append(section)
+
+            if (i - offset) % TO_MERGE != TO_MERGE - 1: continue
+
+            print("Merging", len(previous_sections))
+
+#            final_palette = nbtlib.tag.Compound()
+            final_palette = []
+
+            total_palette = {}
+            palette_mappings = {}
+
+            for section in previous_sections:
+                for element in section["block_states"]["palette"]:
+                    s = str(element)
+                    if s not in total_palette:
+                        total_palette[s] = element
+                        palette_mappings[s] = len(palette_mappings)
+                print("Palette:", len(section["block_states"]["palette"]))
+
+            print("Total palette length:", len(total_palette))
+
+            bits_per_block_final = max(4, math.ceil(math.log2(len(total_palette))))
+            final_storage = SimpleBitStorage(bits_per_block_final, 4096 * TO_MERGE)
+
+            for j, section in enumerate(previous_sections):
+                mappings = []
+                for element in section["block_states"]["palette"]:
+                    s = str(element)
+                    mappings.append(palette_mappings[s])
+                print(mappings)
+
+                palette_size = len(section["block_states"]["palette"])
+                bits_per_block = max(4, math.ceil(math.log2(palette_size)))
+                int_list = section["block_states"]["data"].tolist()
+                storage = SimpleBitStorage(bits_per_block, 4096, int_list)
+
+                decoded_values = []
+                storage.get_all(lambda value: decoded_values.append(value))
+
+                for v in range(4096):
+                    final_storage.set(v + 4096 * j, mappings[decoded_values[v]])
+
+            print(final_storage.get_raw())
+
+
+
+#            section["block_states"]["data"] = nbtlib.tag.LongArray(final_storage.get_raw())
 
         chunk.from_nbtlib(nbt)
 
